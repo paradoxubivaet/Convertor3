61bef1fd4e68a3a82a2173c06dc38019a558d51c
BlackBroke <paradox.ubivaet@mail.ru>
2021-11-24 16:48:31 +0800

    Добавьте файлы проекта.

---
+++ Convertor3/ConvertFromJson.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using System.Dynamic;
+using System.IO;
+
+namespace Convertor3
+{
+    public class ConvertFromJson : IConvertFrom
+    {
+        public List<dynamic> Convert(string path)
+        {
+            //JArray jobj = JsonConvert.DeserializeObject<JArray>(File.ReadAllText(path));
+            //List<dynamic> jlist = jobj.ToObject<List<dynamic>>();
+            //return jlist;
+
+            List<dynamic> list = JsonConvert.DeserializeObject<List<dynamic>>(File.ReadAllText(path));
+            return list;
+        }
+    }
+}
---
+++ Convertor3/Program.cs
@@ -0,0 +1,64 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Convertor3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonPath = string.Empty;
+            string csvPath = string.Empty;
+            string separator = string.Empty;
+            Encoding encoding = Encoding.Default;
+            string que = string.Empty;
+
+            OptionSet CmdParser = new OptionSet
+            {
+                {"i|jsonPath=", "JsonPath", jp => jsonPath = jp },
+                {"o|csvPath=", "CsvPath", cp => csvPath = cp },
+                {"s|separator=", "Separator", sp => separator = sp},
+                {"e|encoding=", "Encoding",  en => encoding = Encoding.GetEncoding(en) },
+                {"q|que=", "Que", q => que = q }
+            };
+
+            try
+            {
+                List<string> Args = CmdParser.Parse(args);
+                if (Args.Count > 1)
+                {
+                    Console.Write($"Unrecognised argument in: {string.Join(" ", Args)}");
+                }
+            }
+            catch (OptionException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (csvPath == string.Empty)
+            {
+                csvPath = Path.Combine(Path.GetDirectoryName(jsonPath) + Path.GetFileNameWithoutExtension(jsonPath) + ".csv");
+            }
+
+            ConvertFromJson convertFromJson = new ConvertFromJson();
+            List<dynamic> objects = convertFromJson.Convert(jsonPath);
+
+
+            ConvertToCsv convertToCsv = new ConvertToCsv();
+            convertToCsv.Convert(objects, csvPath, separator, encoding);
+
+            if (!args.Contains("-q"))
+            {
+                Console.WriteLine("Программа была успешно выполнена");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
